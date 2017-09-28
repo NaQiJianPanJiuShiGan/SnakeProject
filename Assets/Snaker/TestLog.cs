ab024352962809c47734d95625e25631508ffb37
Yinhang <yinhang_joy@sina.com>
2017-09-28 18:06:33 +0800

    no message

---
+++ Assets/Snaker/TestLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SGF;
+
+public class TestLog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debuger.EnableLog = true;
+        //Debuger.EnableSave = true;
+        this.Log("调试测试");
+        this.LogError("调试测试");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
